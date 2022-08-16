799258638dab69cceb7b4c1ff26a3fd88ee46cb8
nnnayy <kny5439@cau.ac.kr>
2022-08-16 19:42:49 +0900

    feat/reporter-sidebar

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import { Navbar } from '../components/Navbar'
+import ReporterSidebar from '../components/ReporterSidebar'
 
 const Home = () => {
   return (
     <>
       <Navbar />
+      <ReporterSidebar />
       <h1>Landing Page</h1>
     </>
   )
