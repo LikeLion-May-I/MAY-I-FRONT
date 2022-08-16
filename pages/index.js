f5b5d789dfbd05a6e551ff98159ee20b30c92a7f
nnnayy <kny5439@cau.ac.kr>
2022-08-16 20:20:42 +0900

    feat/expert-list

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
+import ExpertList from '../components/ExpertList'
 import { Navbar } from '../components/Navbar'
 
 const Home = () => {
   return (
     <>
       <Navbar />
+      <ExpertList />
       <h1>Landing Page</h1>
     </>
   )
