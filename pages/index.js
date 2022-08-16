dd86f34b567b6bce0109458fb0862c17de3b4c7a
nnnayy <kny5439@cau.ac.kr>
2022-08-16 19:58:00 +0900

    feat/reporter-list

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import { Navbar } from '../components/Navbar'
+import ReporterList from '../components/ReporterList'
 
 const Home = () => {
   return (
     <>
       <Navbar />
+      <ReporterList />
       <h1>Landing Page</h1>
     </>
   )
